44eb0f6c121dfc7dd15d3b34218df14fb5ddd90a
Anton <anton.mois33v@gmail.com>
2019-03-29 15:34:21 -0400

    Part 1 complete

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  const candidateList = document.getElementById('election_results');
+  axios.get('https://bb-election-api.herokuapp.com/').then((response) => {
+    response.data.candidates.forEach((candidate) => {
+      const newItem = document.createElement('li');
+      const candidateString = `Name: ${candidate.name}\nVotes: ${candidate.votes}`;
+      newItem.innerText = candidateString;
+      candidateList.append(newItem);
+    });
+  })
 });
